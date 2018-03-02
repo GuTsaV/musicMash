e1e4bf81d1f0be33178f6efd6b26430e6328fa06
Gustav Ehrenborg <gustav.ehrenborg@valtech.se>
2018-03-02 10:11:19 +0100

    Added configuration

---
+++ musicMash/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace musicMash
+{
+    public static class Configuration
+    {
+        static Configuration()
+        {
+            MusicBrainzUrl = Startup.Configuration["urls:musicBrainzUrl"];
+            WikipediaUrl = Startup.Configuration["urls:wikipediaUrl"];
+            CoverArtUrl = Startup.Configuration["urls:coverArtUrl"];
+        }
+
+        public static string MusicBrainzArtistUrl(string artistId) => $"{MusicBrainzUrl}/artist/{artistId}?fmt=json&inc=url-rels+release-groups";
+        public static string WikipediaPageUrl(string pageName) => $"{WikipediaUrl}?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles={pageName}";
+        public static string CoverArtAlbumUrl(string albumId) => $"{CoverArtUrl}/{albumId}";
+
+        private static readonly string MusicBrainzUrl;
+        private static readonly string WikipediaUrl;
+        private static readonly string CoverArtUrl;
+    }
+}
--- musicMash/Startup.cs
+++ musicMash/Startup.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace musicMash
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+            app.UseStatusCodePages();
+            app.UseMvc();
         }
     }
 }
