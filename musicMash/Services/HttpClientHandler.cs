47c4b08b69d5ff2f18830d84f54fb60222fa733d
Gustav Ehrenborg <gustav.ehrenborg@valtech.se>
2018-03-02 10:14:53 +0100

    Added abstraction to HttpCLient

---
+++ musicMash/Services/HttpClientHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace musicMash.Services
+{
+    public class HttpClientHandler : IHttpHandler
+    {
+        private HttpClient _client = new HttpClient();
+
+        public HttpClientHandler(){
+            // Headers are needed to get the correct type of result, that is a JSON result
+            _client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+            _client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+    }
+}
---
+++ musicMash/Services/IHttpHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace musicMash.Services
+{
+    public interface IHttpHandler
+    {
+        Task<HttpResponseMessage> GetAsync(string url);
+    }
+}
